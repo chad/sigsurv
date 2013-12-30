f6742db5a4568038c229dd9a9f9581c61744c0fb
Chad Fowler <chad@chadfowler.com>
2013-12-30 11:18:21 +0100

    match only full words

--- lib/sigsurv.rb
+++ lib/sigsurv.rb
@@ -5,7 +5,8 @@ module Sigsurv
   def self.analyze(directory)
     Dir["#{directory}/**/*\.rb"].map do |file_path|
       contents = IO.read(file_path)
-      [file_path, contents.scan(/#{SYMBOLS.keys.join("|")}/).map{|x| SYMBOLS[x.to_sym]}.join]
+      regex = Regexp.new(SYMBOLS.keys.map{|k| "\\b#{k}\\b"}.join("|"))
+      [file_path, contents.scan(regex).map{|x| SYMBOLS[x.to_sym]}.join]
     end
   end
 end
--- lib/sigsurv/version.rb
+++ lib/sigsurv/version.rb
@@ -1,3 +1,3 @@
 module Sigsurv
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
 end
