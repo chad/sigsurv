b28d2ca4c70ea8b78b1fe2245423229cf3d944a4
Chad Fowler <chad@chadfowler.com>
2013-12-30 10:51:01 +0100

    only .rb files + differentiate do and def

--- lib/sigsurv.rb
+++ lib/sigsurv.rb
@@ -1,11 +1,11 @@
 require "sigsurv/version"
 
 module Sigsurv
-  SYMBOLS = %w(module class def do end)
+  SYMBOLS = {module: "m", class: "c", def: "d", do: "o", e: "e"}
   def self.analyze(directory)
-    Dir["#{directory}/**/*rb"].map do |file_path|
+    Dir["#{directory}/**/*\.rb"].map do |file_path|
       contents = IO.read(file_path)
-      [file_path, contents.scan(/#{SYMBOLS.join("|")}/).map{|x| x.split(//).first}.join]
+      [file_path, contents.scan(/#{SYMBOLS.keys.join("|")}/).map{|x| SYMBOLS[x.to_sym]}.join]
     end
   end
 end
